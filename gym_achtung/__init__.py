b298b84bff8c229dc20b9793595d2921bca2e535
erikbohnsack <bohnsack.erik@gmail.com>
2018-10-24 09:56:15 +0200

    Increase time limit

--- gym_achtung/__init__.py
+++ gym_achtung/__init__.py
@@ -3,7 +3,7 @@ from gym.envs.registration import register
 register(
     id='AchtungDieKurve-v1',
     entry_point='gym_achtung.envs:AchtungDieKurve',
-    timestep_limit=1000,
+    timestep_limit=100000,
     reward_threshold=1.0,
     nondeterministic=True,
 )
